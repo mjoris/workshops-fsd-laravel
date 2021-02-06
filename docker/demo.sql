ce0ef47e360004d54203d70be2a212359f493b63
Joris Maervoet <joris.maervoet@odisee.be>
2021-02-06 16:50:09 +0100

    Initial setup of Docker MCE (incl. PHP 8.0) and static slides on Laravel Basics

---
+++ docker/demo.sql
@@ -0,0 +1,7 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+DROP DATABASE IF EXISTS demo_wmfs;
+CREATE DATABASE demo_wmfs DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
+USE demo_wmfs;
