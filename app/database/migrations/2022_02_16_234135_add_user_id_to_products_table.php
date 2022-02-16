d1f3f68418814c770db62e44f93e1a386f2014e6
Joris Maervoet <joris.maervoet@odisee.be>
2022-02-17 00:59:27 +0100

    moving auth update on migrations into clear separate migration

---
+++ app/database/migrations/2022_02_16_234135_add_user_id_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// Migration for concluding demo 'webshop' of 05.auth
+class AddUserIdToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // nullable() is only there for backward compatibility with previous demos
+            $table->foreignId('user_id')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
