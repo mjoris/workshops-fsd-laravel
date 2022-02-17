818604b21a60307704b2f7d76db01c0635c2bcaf
Joris Maervoet <joris.maervoet@odisee.be>
2022-02-17 01:00:42 +0100

    splitting out seeder of webshop demo into 2 clearly distinct versions (without and with auth)

--- app/database/seeders/DemoSeederWithAuth.php
+++ app/database/seeders/DemoSeederWithAuth.php
@@ -5,11 +5,11 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-// Migration for concluding demo 'webshop' of 02.lets.mvc
-class DemoSeeder extends Seeder
+// Migration for concluding demo 'webshop' of 05.auth
+class DemoSeederWithAuth extends Seeder
 {
     /**
      * Run the database seeds.
@@ -24,11 +24,20 @@ class DemoSeeder extends Seeder
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
 
+        // new !
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'theboss@gmail.com',
+            'password' => Hash::make('Azerty123'),
+            'role' => 'admin'
+        ]);
+
         DB::table('products')->insert([
             'name' => 'OnePlus 8',
             'description' => 'Een leuke Smartphone.',
             'price' => '499.99',
             'brand_id' => 1,
+            'user_id' => 1, // new !
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
