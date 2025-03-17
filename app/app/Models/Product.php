cd1df56289ae18a5f6286dbfe4df887f3236a3dc
Joris Maervoet <joris.maervoet@odisee.be>
2025-03-17 19:15:16 +0100

    add demos on Web APIs

--- app/app/Models/Product.php
+++ app/app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -19,4 +20,25 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+
+    // DEMO ON SERIALIZATION
+    //    protected $hidden = ['user_id', 'brand_id'];
+    //
+    //    protected $appends = ['code'];
+    //
+    //    protected function code(): Attribute
+    //    {
+    //        return Attribute::make(
+    //            get: fn (mixed $value, array $attributes) => 'P'.sprintf('%06d', $attributes['id']),
+    //        );
+    //    }
+    //
+    //    protected function casts(): array
+    //    {
+    //        return [
+    //            'created_at' => 'datetime:Y-m-d H:m:s',
+    //            'updated_at' => 'datetime:Y-m-d H:m:s',
+    //        ];
+    //    }
+
 }
